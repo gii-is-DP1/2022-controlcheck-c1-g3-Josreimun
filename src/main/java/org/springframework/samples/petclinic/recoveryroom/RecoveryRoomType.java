1308ea953d5d22c650ea0cda92d14d48fe9dd774
Josreimun <josreimun@alum.us.es>
2022-11-03 17:22:42 +0100

    Entrega examen

--- src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
+++ src/main/java/org/springframework/samples/petclinic/recoveryroom/RecoveryRoomType.java
@@ -1,11 +1,17 @@
 package org.springframework.samples.petclinic.recoveryroom;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class RecoveryRoomType {
-    Integer id;
+@Entity
+public class RecoveryRoomType extends BaseEntity{
+    @Size(min =3, max =50)
     String name;
 }
